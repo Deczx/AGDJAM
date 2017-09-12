dc9167cb1493f833ccc6224e92276e237b2c0c8f
Jeff Kloos <jeffrey.kloos@gmail.com>
2017-09-12 13:11:26 +0200

    button

---
+++ Assets/FloorButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorButton : MonoBehaviour {
+
+    public GameObject door;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        Debug.Log("BUTTON!");
+        door.SetActive(false);
+        door.active = false;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        door.SetActive(true);
+        door.active = true;
+    }
+}
